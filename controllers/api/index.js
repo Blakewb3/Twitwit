7d0af6401f9e9d45ba9d2d077ba2ad178fe87cbc
Son Giang <sgiangt@gmail.com>
2023-10-12 20:17:16 -0600

    added a route for messages and comments

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
 const twitRoutes = require('./twit-routes.js');
 
-router.use('/users', userRoutes);
 router.use('/twit', twitRoutes);
 
 module.exports = router;

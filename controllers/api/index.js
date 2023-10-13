d62c38275cfb159730245c84aa36012ae2983437
Stephen Garcia <garciastephen5280@gmail.com>
2023-10-12 19:18:36 -0600

    I can always update this if more routes come or if they change

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
+const userRoutes = require('./userRoutes');
 const twitRoutes = require('./twit-routes.js');
 
+router.use('/users', userRoutes);
 router.use('/twit', twitRoutes);
 
 module.exports = router;

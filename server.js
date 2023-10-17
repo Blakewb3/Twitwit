520edad2743b6fe456b37ce586a54bd36cd598b3
Son Giang <sgiangt@gmail.com>
2023-10-16 19:05:26 -0600

    fixed server.js

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const path = require('path');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hbs = exphbs.create({});//add helper here
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// Session configuration
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+// Configure express-session
 app.use(session({
   secret: process.env.SESSION_SECRET,
   cookie: {
@@ -29,19 +31,23 @@ app.use(session({
   }),
 }));
 
-app.use(session(sess));
-
-// Set up Handlebars.js
+// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views')); // Specify the views directory
 
-// Static files (CSS, JavaScript, etc.)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-// Start the server
+// Error handling middleware (custom or built-in Express error handling)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`Now listening on http://localhost:${PORT}`);

8252ea4589248d736155615c483f6c098983b655
Son Giang <sgiangt@gmail.com>
2023-10-16 22:52:44 -0600

    backend overhaul

--- controllers/api/twit-routes.js
+++ controllers/api/twit-routes.js
@@ -1,20 +1,38 @@
 const router = require('express').Router();
-const Twit = require('../../models/twit');   
+const { twit } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-// route to create/add a twit using async/await
-router.post('/', async (req, res) => {
-  try { 
-    const twitData = await Twit.create({
-    user_name: req.body.user_name,
-    twit_content: req.body.twit_content,
-  });
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newTwit = await twit.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
 
-  // if the twit is successfully created, the new response will be returned as json
-  res.status(200).json(dishData)
-} catch (err) {
-  res.status(400).json(err);
-}
+    res.status(200).json(newTwit);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const twitData = await twit.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!twitData) {
+      res.status(404).json({ message: 'No Twit found with this id!' });
+      return;
+    }
+
+    res.status(200).json(twitData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,63 +1,14 @@
-const User = require('./user');
-const Twit = require('./twit');
-const Message = require('./message');
-const Comment = require('./comment');
+const User = require('./User');
+const twit = require('./twit');
 
-// Create associations
-User.hasMany(Twit, {
+User.hasMany(twit, {
   foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
-Twit.belongsTo(User, {
-  foreignKey: 'user_id',
+twit.belongsTo(User, {
+  foreignKey: 'user_id'
 });
 
-User.hasMany(Message, {
-  foreignKey: 'sender_id',
-  onDelete: 'CASCADE',
-});
-
-User.hasMany(Message, {
-  foreignKey: 'receiver_id',
-  onDelete: 'CASCADE',
-});
-
-Message.belongsTo(User, {
-  foreignKey: 'sender_id',
-  as: 'sender',
-});
-
-Message.belongsTo(User, {
-  foreignKey: 'receiver_id',
-  as: 'receiver',
-});
-
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
-});
-
-Twit.hasMany(Comment, {
-  foreignKey: 'twit_id',
-  onDelete: 'CASCADE',
-});
-
-Message.hasOne(Comment, {
-  foreignKey: 'message_id',
-  onDelete: 'CASCADE',
-});
-
-Comment.belongsTo(User, {
-  foreignKey: 'user_id',
-});
-
-Comment.belongsTo(Twit, {
-  foreignKey: 'twit_id',
-});
-
-Comment.belongsTo(Message, {
-  foreignKey: 'message_id',
-});
-
-module.exports = { User, Twit, Message, Comment };
+module.exports = { User, twit };
 

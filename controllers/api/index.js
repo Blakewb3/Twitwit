c6d66e92e1982df6d3ef79b5641083e4938c211f
Stephen Garcia <garciastephen5280@gmail.com>
2023-10-17 16:58:02 -0600

    should be able to start

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const twitRoutes = require('./twitRoutes');
+const userRoutes = require('./user-routes');
+const twitRoutes = require('./twit-routes');
 
 router.use('/users', userRoutes);
 router.use('/twits', twitRoutes);

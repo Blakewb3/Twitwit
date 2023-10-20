dcdb85e70900c3072a02c75b92954db1ded9a8d8
Stephen Garcia <garciastephen5280@gmail.com>
2023-10-19 22:48:54 -0600

    removed comment file and updated index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const twitRoutes = require('./twitRoutes');
+const projectRoutes = require('./projectRoutes');
 
 router.use('/users', userRoutes);
-router.use('/twits', twitRoutes);
+router.use('/projects', projectRoutes);
 
 module.exports = router;
